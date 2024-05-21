e9ef1affc7c27f4426b8b505ac298d128a269f9b
M78Code <leonm78coder@gmail.com>
2024-05-21 23:01:30 +0900

    no message

--- pytorch_study/homework/math_homework/question_2_2_7.py
+++ pytorch_study/homework/math_homework/question_2_2_7.py
@@ -18,6 +18,7 @@ def f(x):
 def fourier_func(x):
     return -2 * (np.sin(x))
 
+
 # x = np.arange(0, 360)
 # # 如果打印x, NumPy 会给你很好看的打印格式
 # # print(x)
@@ -29,3 +30,29 @@ def fourier_func(x):
 # pt.title('Sine wave')
 #
 # pt.show()
+
+
+for i in range(1, 1000):
+    sum_ = 0
+    count = 0
+    check_count = 0
+    j_array = []
+    for j in range(1, i):
+
+        if i % j == 0:
+            count += 1
+            check_count += 1
+            sum_ += j
+            j_array.append(j)
+            if sum_ == i:
+                print(f"j = {j},i={i}, j_array={j_array}")
+                j_array.clear()
+            else:
+                check_count += 1
+        else:
+            pass
+    # if x % 2 == 0:
+
+# for ii in range(5):
+#     for jj in range(ii):
+#         print(f"ii = {ii}, jj={jj}")
